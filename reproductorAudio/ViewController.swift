087ded4acacc75826a3c79514dc6aba2fd7c3847
Sebastià Fiol <sfiol@efeefe.com>
2016-09-04 22:04:38 +0200

    UIPicker

--- reproductorAudio/ViewController.swift
+++ reproductorAudio/ViewController.swift
@@ -7,16 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
 
     @IBOutlet weak var imageCover: UIImageView!
     @IBOutlet weak var selectSong: UIPickerView!
     @IBOutlet weak var volumen: UISlider!
+    @IBOutlet weak var titulo: UILabel!
+    
+    var listSongs = ["Shake it off","Knockin' on Heaven's Door","Surfin USA","is this Love","Thunderstruck"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        selectSong.dataSource = self
+        selectSong.delegate = self
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return listSongs.count
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titulo = NSAttributedString(string: listSongs[row], attributes: [NSFontAttributeName:UIFont(name: "Georgia", size: 15.0)!,NSForegroundColorAttributeName:UIColor.blueColor()])
+        return titulo
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        tocarMostrarPortada(row)
+        
     }
 
     override func didReceiveMemoryWarning() {
